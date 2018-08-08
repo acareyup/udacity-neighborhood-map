15a7a14b27033502a565b209f92b7e9799ddd884
unknown <acareyup@gmail.com>
2018-08-08 09:55:26 +0300

    set states and create points

---
+++ src/points.js
@@ -0,0 +1,32 @@
+export const points = [
+  {
+    name : "Hagia Sophia Museum",
+    type : "Museum",
+    latitude : 41.008559,
+    longitude : 28.980026 
+  },
+  {
+    name : "Blue Mosque",
+    type : "Mosque", 
+    latitude : 41.005448,
+    longitude : 28.976820
+  },
+  {
+    name : "Walled Obelisk",
+    type : "Place",
+    latitude : 41.005379,
+    longitude : 28.974848
+  },
+  {
+    name : "Hipodrome",
+    type : "Museum",
+    latitude : 41.005812, 
+    longitude : 28.975122
+  },
+  {
+    name : "Grand Bazaar",
+    type : "Place",
+    latitude : 41.010689,
+    longitude : 28.968067
+  }
+]
