3a9f0048c9277402efb57274f647fef90c420efc
unknown <acareyup@gmail.com>
2018-08-08 10:14:29 +0300

    create google maps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { points } from './points';
 import './App.css';
 
 class App extends Component {
+  //constructor function
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: points,
+      map: "",
+      infoWindow: "",
+      prevmarker: ""
+    }
+  }
+
+  createGoogleMap(){
+    let _this = this;
+    let mapstyle = document.getElementById("map")
+    mapstyle.style.height = window.innerHeight+"px"
+
+    let map = new window.google.maps.Map(mapstyle, {
+      center: {lat:41.007138, lng:28.976709},
+      zoom:12,
+      mapTypeControl:false
+    })
+
+    let infoWindow = new window.google.maps.InfoWindow({});
+    window.google.maps.event.addListener(infoWindow, "closeclick", function(){
+      _this.closeInfo();
+    })
+
+    this.setState({
+      map:map,
+      infoWindow : infoWindow
+    })
+
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    
   }
 }
 
